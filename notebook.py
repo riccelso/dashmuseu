e6d06a1205fe31ab15f7f9add8c8cf74c0a33550
riccelso <ricelsos@outlook.com>
2022-08-24 20:38:17 -0300

    Dashboard - nova estrutura e cads

--- notebook.py
+++ notebook.py
@@ -6,8 +6,7 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 import os, subprocess
-#import gunicorn
-
+from datetime import date
 # pd.options.plotting.backend = "plotly"
 
 eventos = pd.read_parquet("curated/eventos")
@@ -17,13 +16,14 @@ faixaetaria = pd.read_parquet("curated/faixaetaria")
 libras = pd.read_parquet("curated/libras")
 regiao = pd.read_parquet('curated/regiao')
 estados = pd.read_parquet('curated/estados')
+print(estados.head(2))
 
-
+estados['estado'] = estados.estado.str.title()
 
 museu2 = museu[['id_museu', 'regiao', 'latitude', 'longitude']]
 inter = pd.merge(
     museu2, 
-    occ[['spaceId', 'eventId']], 
+    occ.iloc[:, [1, 2, 4, 5, 6, 7, -2, -1]],
     left_on='id_museu', 
     right_on='spaceId', 
     how='right'
@@ -41,9 +41,14 @@ inter['faixa_etaria'] = inter.faixa_etaria.replace(
     dict(faixaetaria.values)).copy()
 inter['traducao_libras'] = inter.traducao_libras.replace(
     dict(libras.values)).copy()
+inter['data_inicio'] = inter.data_inicio.astype(np.datetime64)
+inter['data_fim'] = inter.data_fim.astype(np.datetime64)
 
+inicio = inter.data_inicio.min()
+fim = inter.data_inicio.max()
 
 cor_eventos = px.colors.sequential.Blues[3:]
+cor_museus = 'Viridis'
 
 #px.colors.sequential.swatches()
 
@@ -60,8 +65,35 @@ cor_eventos = px.colors.sequential.Blues[3:]
 #    nº total de eventos, total de museus, estado com + museus, museus com + eventos
 # - abaixar slider
 
+# - Colocar nos cards:
+# dbc.Col(
+#     dbc.CardImg(
+#         src="/static/images/portrait-placeholder.png",
+#         className="img-fluid rounded-start",
+#     ),
+#     className="col-md-4",
+# ),
+# dbc.Col(
+#     dbc.CardBody(
+#         [
+#             html.H4("Card title", className="card-title"),
+#             html.P(
+#                 "This is a wider card with supporting text "
+#                 "below as a natural lead-in to additional "
+#                 "content. This content is a bit longer.",
+#                 className="card-text",
+#             ),
+#             html.Small(
+#                 "Last updated 3 mins ago",
+#                 className="card-text text-muted",
+#             ),
+#         ]
+#     ),
+#     className="col-md-8",
+# ),
+
 # ============= APP
-app = Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])  # SLATE
 server = app.server
 
 # ============= MONTAGEM DE COMPONENTES
@@ -69,6 +101,31 @@ server = app.server
 
 logo = html.Img(
     id='logo',
+    src=app.get_asset_url('proc_museus.png'),
+    style={
+        'height': '80px', 
+        'width': '220px', 
+        'margin-left': '40px',
+        'margin-top': '5px',
+        }
+)
+
+card1 = html.Img(
+    id='card1',
+    src=app.get_asset_url('corn.png'),
+    style={'height': '50px', 'width': '80px', 'margin-left': '60px',
+           'margin-bottom': '20px', 'margin-top': '10px'}
+)  # width = largura
+
+card2 = html.Img(
+    id='card2',
+    src=app.get_asset_url('corn.png'),
+    style={'height': '50px', 'width': '80px', 'margin-left': '60px',
+           'margin-bottom': '20px', 'margin-top': '10px'}
+)  # width = largura
+
+card3 = html.Img(
+    id='card3',
     src=app.get_asset_url('corn.png'),
     style={'height': '50px', 'width': '80px', 'margin-left': '60px',
            'margin-bottom': '20px', 'margin-top': '10px'}
@@ -78,16 +135,17 @@ drop1 = dcc.Dropdown(
     options=[{'label': i, 'value': i}
              for i in sorted(faixaetaria.faixa_etaria.unique())],
     id='drop_idade',
-    placeholder='Clique para selecionar',
-    #value='18 anos'
+    placeholder='Habilitado somente para os eventos',
+    disabled=True,
 )
+
 
 drop2 = dcc.Dropdown(
     options=[{'label': i, 'value': i}
              for i in libras.trad_libras.unique()[-1:0:-1]],
     id='assistencia',
-    placeholder='Clique para selecionar',
-    #value='Sim'
+    placeholder='Habilitado somente para os eventos',
+    disabled=True,
 )
 
 drop3 = dcc.Dropdown(
@@ -103,28 +161,25 @@ drop3 = dcc.Dropdown(
 check = dcc.RadioItems(
     id='check',
     options=[
-        {'label': i, 'value': i}
-        for i in ['Eventos por museu', 'Localização dos museus']
+        {
+            'label': i, 
+            'value': i
+        }
+        for i in [' Eventos por museu', ' Localização dos museus']
     ],
-    value='Localização dos museus',
+    value=' Localização dos museus',
 )
 
 bt1 = dbc.Button('Resetar filtros', id='btn1', color="secondary",
-                 size="sm", style={'margin-left': '0px'})
+                 size="sm", style={'margin-left': '0px', 'margin-bottom':'20px'})
 
-slicer = dcc.Slider(
-    min=0,
-    max=10,
-    step=2.5,
-    value=10,
-    id='my-slider',
-    marks={
-        0: '0°F',
-        2.5: '2.5°F',
-        5: '5°F',
-        7.5: '7.5°F',
-        10: {'label': '10°F', 'style': {'color': '#f50'}}
-    },
+date_range = dcc.DatePickerRange(
+    id='date_range',
+    min_date_allowed=inicio,
+    max_date_allowed=fim,
+    start_date=inicio,
+    end_date=fim,
+    display_format='DD/MM/YYYY',
 )
 
 # COLUNA 2
@@ -139,81 +194,171 @@ fig.update_layout(
     showlegend=False
 )
 
-# slicer = dcc.Graph(id='slicer', figure=fig)
-
 # VARIÁVEIS AUXILIARES
 
 dist_labdrops = '15px'
-
-
+teto = '15px'
 
 # ============= LAYOUT
 
-app.layout = dbc.Container([
+app.layout = html.Div([
     dbc.Row([
         dbc.Col([  # COLUNA 1
-            dbc.Row([logo], style={'margin-top': '30px'}),
 
+                html.Div([
+                    dbc.Card(
+                        logo,
+                        color='primary',
+                        inverse=True,
+                        style={'margin-top':teto}
+                    ),
+                ]), # style={'margin-right': '50px'}
+                    # color='primary',
+            dbc.Card([
             html.Div([
-                # CAIXA 1
-                html.Label(children=['Filtro de faixa etária:'], id='l1'),
-                # DROPDOWN 1
-                dbc.Col([drop1], style={'margin-bottom': dist_labdrops}),
+
+                #html.Label('Filtrar data:'),
+                html.Div([dbc.Row([date_range])], style={
+                    'font-size': '1px',
+                    'border': '1px solid #ccc',
+                    'margin-right': '16px',
+                    'margin-bottom': dist_labdrops,
+                        }
+                    ),
+
+                html.Div([
+                    # CAIXA 1
+                    html.Label(children=['Filtro de faixa etária:'], id='l1'),
+                    
+                    # DROPDOWN 1
+                    dbc.Col([drop1], style={
+                        'margin-bottom': dist_labdrops, 
+                        'padding-right':'11px'
+                        }),
+                ]),
 
                 # CAIXA 2
                 html.Label(children=['Interprete de Libras:'], id='l2'),
                 # DROPDOWN 2
-                dbc.Col([drop2], style={'margin-bottom': dist_labdrops}),
+                dbc.Col([drop2], style={
+                    'margin-bottom': dist_labdrops,
+                    'padding-right':'11px'
+                    }),
 
                 html.Label(children=['Filtro por Estado:'],
                            id='l3'),  # CAIXA 3
                 # DROPDOWN 3
-                dbc.Col([drop3], style={'margin-bottom': dist_labdrops}),
+                dbc.Col([drop3], style={
+                    'margin-bottom': dist_labdrops,
+                    'padding-right': '11px'
+                    }),
 
                 html.Label(children=['Tipos de visualização:'], id='l4', style={
                            'margin-top': '15px'}),  # CAIXA 4
                 dbc.Row([check], style={'margin-top': '1px'}),
-            ], style={'margin-top': '50px'}),
-            html.Div(bt1, style={'margin-top': '32px'}),
+            ], style={
+                'margin-top': '22px',
+                'margin-left':'10px'
+                }),
+            
+            html.Div(bt1, style={
+                'margin-top': '8px',
+                'margin-left': '10px'
+                }),
+            ],
+            color='secondary',
+            inverse=True,
+            outline=True,
+            style={
+                'width':'300px', 
+                'height':'76vh',
+                'margin-top':'40px',
+                }
+            )
 
-        ], sm=3),
+        ]), #sm=3),
 
         dbc.Col([  # COLUNA 2
-
-            dbc.Row([slicer], style={'height': '10vh',
-                    'margin-top': '50px'}),  # C2 L1
+            
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card(
+                        card1,
+                        color='primary',
+                        inverse=True,
+                        className='cards'
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Card(
+                        card2,
+                        color='primary',
+                        inverse=True,
+                        className='cards'
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Card(
+                        card3,
+                        color='primary',
+                        inverse=True,
+                        className='cards'
+                    ),
+                ])
+            ], style={'margin-top':teto, 'margin-left':'10px'}),
+            
             dbc.Row([mapa], style={
-                    'margin-top': '20px', 'margin-right': '1px', 'height': '75vh'})  # C2 L2
+                    'margin-top': '20px',
+                    'margin-bottom': '20px',
+                    'margin-left': '90px', 
+                    'height': '80vh',
+                    }
+                )  # C2 L2
 
         ], sm=9),
     ])
-])
+], style={'margin':'30px', 'margin-top':'0px'})
 
 
 @app.callback(
     Output('drop_idade', 'value'),
     Output('assistencia', 'value'),
     Output('estados', 'value'),
+    Output('date_range', 'start_date'),
+    Output('date_range', 'end_date'),
     Input('btn1', 'n_clicks'),
 )
 def limpar_filtros(btn1):
-    return None, None, None
+    return None, None, None, inicio, fim
+
 
 
 @app.callback(
     Output('mapa', 'figure'),
+    Output('drop_idade', 'placeholder'),
+    Output('assistencia', 'placeholder'),
+    Output('assistencia', 'disabled'),
+    Output('drop_idade', 'disabled'),
     Input('drop_idade', 'value'),
     Input('assistencia', 'value'),
     Input('estados', 'value'),
     Input('btn1', 'n_clicks'),
     Input('check', 'value')
 )
-def ver_resultado(drop_idade, drop_assistencia, drop_estado, btn1, check):
-    global last_lon, last_lat
-
-    # filtrando dataframe
-    temp = inter.copy()
+def config_dados(drop_idade, drop_assistencia, drop_estado, btn1, check):
+    # Verificando ação de reset
+    check = check.strip()
     reset = callback_context.triggered[0]['prop_id'] == 'btn1.n_clicks'
+
+    if check == 'Eventos por museu':
+        placeholder = 'Clique para selecionar'
+        disabled = False
+    else:
+        placeholder = 'Habilitado somente para os eventos'
+        disabled = True
+
+    # Filtrando dataframe
+    temp = inter.copy()
 
     if not drop_idade is None and not reset:
         temp = temp[temp.faixa_etaria == drop_idade]
@@ -224,13 +369,15 @@ def ver_resultado(drop_idade, drop_assistencia, drop_estado, btn1, check):
     temp = temp[['id_museu', 'eventId']].groupby(
         'id_museu', as_index=None).count()
     temp.rename(columns={'eventId': 'contagem_eventos'}, inplace=True)
-
+    
     temp = pd.merge(
         museu,
         temp,
         on='id_museu',
         how='left'
-    )[['latitude', 'longitude', 'regiao', 'contagem_eventos', 'estado_completo']]
+    )
+    
+    temp = temp[['latitude', 'longitude', 'regiao', 'contagem_eventos', 'estado_completo']]
 
     temp['estado_completo'] = temp['estado_completo'].replace(
         dict(estados[['id_estados', 'estado']].values)).copy()
@@ -253,16 +400,16 @@ def ver_resultado(drop_idade, drop_assistencia, drop_estado, btn1, check):
         zoom, centro = 3.35, dict(lat=-13.904, lon=-49.747)
     else:
         if check == 'Localização dos museus':
-            centro = dict(lat=temp.latitude.mean()+2.3,
+            centro = dict(lat=temp.latitude.mean(),
                           lon=temp.longitude.mean())
-            zoom = 3 if drop_estado is None else 4
+            zoom = 3 if drop_estado is None else 5
         elif check == 'Eventos por museu':
-            centro = dict(lat=temp.latitude.mean()+4,
+            centro = dict(lat=temp.latitude.mean(),
                           lon=temp.longitude.mean())
-            zoom = 3.2 if drop_estado is None else 4
+            zoom = 3.2 if drop_estado is None else 6
 
     # DEFININDO MAPA
-    cor = cor_eventos if check == 'Eventos por museu' else 'Viridis'
+    cor = cor_eventos if check == 'Eventos por museu' else cor_museus
 
     fig_1 = px.scatter_mapbox(
         temp,
@@ -300,12 +447,26 @@ def ver_resultado(drop_idade, drop_assistencia, drop_estado, btn1, check):
     fig_1.update_coloraxes(showscale=True)
     fig_1.layout.coloraxis.colorbar.title = 'Eventos'
 
-    # fig_1.update(layout_coloraxis_showscale=False)
-    # fig_1.layout.coloraxis.showscale = False
-    #fig_1.update_traces(marker_showscale=False)
-    #fig_1.update_layout(mapbox_style='mapbox://styles/ricelsos/cl756fj5w001814n2bz85vj3h')
+    fig.update_layout(
+        hoverlabel=dict(
+            bgcolor="white",
+            font_size=16,
+            font_family="Rockwell"
+        )
+    )
 
-    return fig_1
+    #hover_name = "country", 
+    # hover_data = ["continent", "pop"]
+
+    # hovertemplate =
+    # '<i>Price</i>: $%{y:.2f}'+
+    # '<br><b>X</b>: %{x}<br>'+
+    # '<b>%{text}</b>',
+
+    # hovertemplate="%{label}: <br>Popularity: %{percent} </br> %{text}"
+
+
+    return fig_1, placeholder, placeholder, disabled, disabled
 
 
 @app.callback(
@@ -313,6 +474,7 @@ def ver_resultado(drop_idade, drop_assistencia, drop_estado, btn1, check):
     Input('check', 'value')
 )
 def mudar_opcoes(check):
+    check = check.strip()
     temp = inter.copy()
     temp = temp[['id_museu', 'eventId']].groupby(
         'id_museu', as_index=None).count()
@@ -335,4 +497,4 @@ def mudar_opcoes(check):
     return [{'label': i, 'value': i} for i in estados.estado.values]
 
 if __name__=='__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=8899)
